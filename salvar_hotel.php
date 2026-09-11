93195105a021cbed0c3aa6438d998b7f9b9aa2d3
Marcilio <0000324467@senaimgaluno.com.br>
2026-09-10 21:52:18 -0300

    criação de codigos e reformulação de outros

---
+++ salvar_hotel.php
@@ -0,0 +1,16 @@
+<?php
+require_once "conexao.php";
+
+$nome = $_POST['hotel'];
+$cidade = $_POST['cidade'];
+$estrelas= $_POST['estrelas'];
+
+$sql = "INSERT INTO hoteis (nome, cidade, estrelas)
+VALUES ('$nome', '$cidade', '$estrelas')";
+
+if(mysqli_query($conexao, $sql)){
+
+}else{
+
+}
+?>
