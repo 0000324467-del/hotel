861114e2f277f8eaf8d2c26a89f34d642845aeac
Marcilio <0000324467@senaimgaluno.com.br>
2026-09-15 21:47:21 -0300

    alterações em 15 de setembro

--- salvar_hotel.php
+++ salvar_hotel.php
@@ -1,12 +1,13 @@
 <?php
+
 require_once "conexao.php";
 
-$nome = $_POST['hotel'];
+$nome = $_POST['nome'];
 $cidade = $_POST['cidade'];
 $estrelas= $_POST['estrelas'];
 
 $sql = "INSERT INTO hoteis (nome, cidade, estrelas)
-VALUES ('$nome', '$cidade', '$estrelas')";
+VALUES ('$nome', '$cidade', $estrelas)";
 
 if(mysqli_query($conexao, $sql)){
 
