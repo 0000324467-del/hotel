cf484af40f0861ce2a1feaeea4f7fe88b96bf98c
Marcilio <0000324467@senaimgaluno.com.br>
2026-09-17 20:44:31 -0300

    novos arquivos e atualizações

--- autenticar_hotel.php
+++ autenticar_hotel.php
@@ -1,3 +1,19 @@
 <?php
 
+include "conexao.php"; 
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM hoteis WHERE email = '$email' and senha = '$senha'";
+$resultado = mysqli_query($conexao, $sql);
+
+if (mysqli_num_rows($resultado) > 0) {
+    header("location: cadastrar_quarto.php");
+    exit();
+}else {
+    header("location: login_hotel.html");
+    exit();
+}
+
 ?>
